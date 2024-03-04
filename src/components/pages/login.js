6a5d2d3b9faa78cf550cf76dcc92552c6a0535ae
Lucas Sena <lucassenasilvamachado@gmail.com>
2024-03-04 18:42:21 -0300

    login bug correction pt2

---
+++ src/components/pages/login.js
@@ -0,0 +1,47 @@
+import styles from "./login.module.css";
+import carro from "../img/bmw.png";
+import React, { useState } from "react";
+
+function Login() {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
+  function logar(e) {
+    e.preventDefault();
+
+    console.log("User:", user, " Password:", password);
+  }
+  return (
+    <body>
+      <div className={styles.container}>
+        <div className={styles.form_container}>
+          <form className={styles.form} onSubmit={logar}>
+            <header>
+              <h1>LOGIN</h1>
+              <span>Seja bem vindo ao CarTrack</span>
+            </header>
+            <input
+              type="number"
+              placeholder="Usuário"
+              onChange={(e) => setUser(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Senha"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <a href="/">Esqueceu sua senha?</a>
+            <button>Entrar</button>
+          </form>
+        </div>
+        <div>
+          <div className={styles.overlay_panel}>
+            <img src={carro} alt="Carro" />
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+}
+
+export default Login;
