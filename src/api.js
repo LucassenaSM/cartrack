db471bfc0d745092bdffc18df8f3b828f7a652be
Lucas Sena <lucassenasilvamachado@gmail.com>
2024-06-19 18:51:57 -0300

    Correção no back-end

--- src/api.js
+++ src/api.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 
-const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://cartrack-rosy.vercel.app' 
-  : 'http://localhost:3030'; 
+const API_URL = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:3030' 
+  : 'https://cartrack-server.onrender.com'; 
 
 export const getLogins = () => axios.get(`${API_URL}/`);
 
-export const login = (username, password) => axios.post(`${API_URL}/api/login`, { username, password });
+export const login = (username, password) => axios.post(`${API_URL}/login`, { username, password });
 
-export const updateSessionToken = (username, sessionToken) => axios.post(`${API_URL}/api/updateSessionToken`, { username, sessionToken });
+export const updateSessionToken = (username, sessionToken) => axios.post(`${API_URL}/updateSessionToken`, { username, sessionToken });
 
-export const readSessionToken = (sessionToken) => axios.post(`${API_URL}/api/readSessionToken`, { sessionToken });
+export const readSessionToken = (sessionToken) => axios.post(`${API_URL}/readSessionToken`, { sessionToken });
 
-export const getUserBySessionToken = (sessionToken) => axios.post(`${API_URL}/api/user`, { sessionToken });
+export const getUserBySessionToken = (sessionToken) => axios.post(`${API_URL}/user`, { sessionToken });
 
-export const getUsuarios = () => axios.get(`${API_URL}/api/getUsuarios`);
+export const getUsuarios = () => axios.get(`${API_URL}/getUsuarios`);
